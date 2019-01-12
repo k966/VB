f0d1306f3c521c0067ceba3b59491eb63db14814
k966 <42845432+k966@users.noreply.github.com>
2019-01-12 18:27:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(` New Bot .`,'https://www.twitch.tv/v5bz');
+  client.user.setGame(` Type !bc .`,'https://www.twitch.tv/v5bz');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
